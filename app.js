67d090c0c243adf215739295c1d7c4fd196c54a9
Home <praveen.sahu7512@gmail.com>
2023-01-29 07:29:52 -0500

    additional comment added

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express")
 const https = require("https")
 const bodyParser = require("body-parser")
 
+//express libary included
+
+
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//basic http methods are used over here
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
